aee99aa74aba4e8e5a937331d751e54cbb24b8e8
kaenozu <kaenozu@gmail.com>
2023-02-21 17:33:08 +0900

    TemplateMethod

---
+++ TemplateMethod/CharDisplay.java
@@ -0,0 +1,26 @@
+package TemplateMethod;
+
+public class CharDisplay extends AbstractDisplay {
+
+    private char ch;
+
+    public CharDisplay(char ch) {
+        this.ch = ch;
+    }
+
+    @Override
+    void open() {
+        System.out.print("<<");
+    }
+
+    @Override
+    void print() {
+        System.out.print(ch);
+    }
+
+    @Override
+    void close() {
+        System.out.println(">>");
+    }
+
+}
