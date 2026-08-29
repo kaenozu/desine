02c93db72b36322e252625666f5babe14e733fb7
kaenozu <106388957+kaenozu@users.noreply.github.com>
2026-08-29 19:02:11 +0900

    fix: propagate AdapterTest IO failures (#2 #3)

--- AdapterTest/Main.java
+++ AdapterTest/Main.java
@@ -1,20 +1,19 @@
 package AdapterTest;
 
 import java.io.IOException;
+import java.nio.file.Path;
 
 public class Main {
-    public static void main(String[] args) {
-        FileIo f = new FileProperties();
-
-        try {
-            f.readFromeFile("file.txt");
-            f.setValue("year", "2004");
-            f.setValue("month", "4");
-            f.setValue("day", "2 1");
-            f.writeToFile("newfile.txt");
-        } catch (IOException e) {
-            e.printStackTrace();
+    public static void main(String[] args) throws IOException {
+        if (args.length != 1) {
+            throw new IllegalArgumentException("Usage: AdapterTest.Main <base-directory>");
         }
 
+        FileIo f = new FileProperties(Path.of(args[0]));
+        f.readFromeFile("file.txt");
+        f.setValue("year", "2004");
+        f.setValue("month", "4");
+        f.setValue("day", "2 1");
+        f.writeToFile("newfile.txt");
     }
 }
